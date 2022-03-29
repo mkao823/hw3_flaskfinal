f6b692d469c1efb681e3976330dd3245b6078732
Michael Kao <michael.kao@sjsu.edu>
2022-03-28 22:26:51 -0700

    Final submission

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+myobj = Flask(__name__)
+myobj.config.from_mapping(SECRET_KEY='password')
+from app import routes
+
---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import myobj
+from flask import render_template, flash
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
+
+
+#name = 'Lisa'
+city_names = ['Paris', 'London', 'Rome', 'Tahiti']
+
+class LoginForm(FlaskForm):
+    city = StringField('City Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@myobj.route('/', methods=['GET', 'POST'])
+def home():
+    form = LoginForm()
+    if form.validate_on_submit:
+        flash(form.city.data)
+        #print(f'{form.city.data}')
+    return render_template('home.html', name = 'Lisa', city_names = city_names, form = form)
+    #home.html and name
+
